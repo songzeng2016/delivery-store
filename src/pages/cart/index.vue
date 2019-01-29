03ef9f6582ea174da686cf084daa427b0f1c5d3a
songzeng2016 <songzeng62@gmail.com>
2019-01-29 16:08:08 +0800

    update cart

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="cart">
-
+    <div class="empty">
+      <img class="icon" src="/static/images/icon_cart_empty.png" alt="">
+      <p class="desc">购物车空空如也~</p>
+    </div>
   </div>
 </template>
 
@@ -17,4 +20,12 @@
 <style lang="stylus" type="text/stylus" scoped>
   .cart
     background: #f4f4f4
+    .empty
+      padding: 20px 0
+      text-align: center
+      .icon
+        width: 150px
+        height: 150px
+      .desc
+        color: #666666
 </style>
